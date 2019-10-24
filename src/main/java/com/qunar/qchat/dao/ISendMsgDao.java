e9c19fdcac2e1ddbd0f8cd0268f7b11796a5a59c
botaom.ma <botaom.ma@qunar.com>
2019-10-24 20:17:51 +0800

    增加获取用户名片和http接口发送消息接口

--- src/main/java/com/qunar/qchat/dao/ISendMsgDao.java
+++ src/main/java/com/qunar/qchat/dao/ISendMsgDao.java
@@ -1,27 +1,12 @@
 package com.qunar.qchat.dao;
 
 import com.qunar.qchat.model.SendWhiteModel;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @Component
 public interface ISendMsgDao {
 
     void insertSelective(SendWhiteModel sendWhiteModel);
-
-    SendWhiteModel selectByappcode(@Param("appcode") String appcode);
-
-    SendWhiteModel selectByFromUser(@Param("fromUser") String fromUser);
-
-    List<SendWhiteModel> selectReview();
-
-    void deleteWhiteById(@Param("id") Integer id);
-
-    void updateReviewFlag(@Param("review") Integer REVIEW, @Param("id") Integer id);
-
-    SendWhiteModel selectById(@Param("id") Integer id);
-
 
 }
